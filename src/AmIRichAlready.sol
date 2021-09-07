b0504ab16a8b2ebf2fcdfa5041a5cdb0544365e1
test test <testaccount@testaccount.testaccount>
2021-09-07 21:18:43 +0200

    Included askRoot functions.

--- src/AmIRichAlready.sol
+++ src/AmIRichAlready.sol
@@ -5,8 +5,14 @@ interface IERC20 {
 }
 
 contract AmIRichAlready {
-    IERC20 private tokenContract;
+    // define original variables
+	IERC20 private tokenContract;
     uint public richness = 1000000 * 10 ** 18;
+
+	// define askroot variables
+	bool solved;    // Boolean to denote if contract is solved.
+    address payable owner;  // Owner of the contract, first this is the sponser.
+    uint expiry;        // Get the time when the contract expires.
 
     constructor (IERC20 _tokenContract) public {
         tokenContract = _tokenContract;
@@ -17,8 +23,47 @@ contract AmIRichAlready {
         return balance > richness;
     }
 
-    // IS THIS NEEDED???
+    // IS THIS NEEDED???: NO
     function setRichness(uint256 _richness) public {
       richness = _richness;
     }
+	
+	
+	// Include askroot functions
+	//function test(address payable hunter) public payable {
+	function different_name(address payable hunter) public payable {
+        TemplateSolveContract solver = TemplateSolveContract(msg.sender); // The message sender is the contract activating the test function.
+        uint x = 100;                                   // Sample input.
+        uint16 y = 10;                                  // Sample expected output.
+        require(y == solver.main(x), "Wrong output");   // Require the output of the main function to be y.
+        solved = true;                                  // Set solved to true.
+        owner = hunter;                                 // Set the ownership to the hunter.
+        owner.transfer(address(this).balance);          // Transfer the bounty to the hunter.
+    }
+
+    // Getter function for the solved variable.
+    function getSolved() public view returns (bool){   
+        return solved;
+    }
+    
+    // Getter function for the Ownership.
+    function getOwner() public view returns (address) { 
+        return owner;
+    }
+    
+    // Getter function for the balance of the contract.
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+    // Refund method to claim the value of the contract after expiry.
+    function refund() public {
+        require(msg.sender == owner && block.timestamp >= expiry, "Contract is not expired yet");   // The sender must own the contract and the contract must be expired.
+        selfdestruct(owner);    // Let the contract selfdestruct and move the value to the owner.
+    }
+}
+
+// TemplateSolveContract so the TestContract knows the structure of the SolveContract.
+abstract contract TemplateSolveContract {
+	function main(uint x) public virtual returns (uint);
 }
