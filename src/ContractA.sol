3c9eb5feb0a333735d09d5b87d4a605f1014a828
test test <testaccount@testaccount.testaccount>
2021-09-08 23:17:30 +0200

    Updated to compiler 0.6.6.

--- src/ContractA.sol
+++ src/ContractA.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.2;
+pragma solidity ^0.6.6;
 
 //import 'ContractB.sol';
 import "src/ContractB.sol";
--- src/SolveContract.sol
+++ src/SolveContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 //pragma solidity >=0.4.16 <0.9.0;
-pragma solidity ^0.6.2;
+pragma solidity ^0.6.6;
 
 
 abstract contract InterfaceB{
