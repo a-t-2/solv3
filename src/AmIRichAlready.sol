0d958a29bc8e171455b4ea8b68354bec909db9cc
test test <testaccount@testaccount.testaccount>
2021-09-09 04:44:12 +0200

    mem.

--- src/AmIRichAlready.sol
+++ src/AmIRichAlready.sol
@@ -85,7 +85,7 @@ contract AmIRichAlready {
 		return n.helloUniverse();
 	}
 	
-	function callUintSmallSquareFromVRFContract(address randomNumberConsumerAddres) public returns(uint16) {
+	function callUintSmallSquareFromVRFContract(address randomNumberConsumerAddres) public returns(uint16 memory) {
 		InterfaceRandomN p = InterfaceRandomN(randomNumberConsumerAddres);
 		uint256 square = p.returnSomeSquare();
 		uint16 smallSquare = uint16(square);
